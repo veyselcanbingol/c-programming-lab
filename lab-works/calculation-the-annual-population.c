7d2219dc07a103746c7cf6df65fc3dd6217aa1ff
veyselcanbingol <79760954+veyselcanbingol@users.noreply.github.com>
2021-11-20 15:36:24 +0300

    Create calculation-the-annual-population.c

---
+++ lab-works/calculation-the-annual-population.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    // CALCULATION THE ANNUAL POPULATION 
+
+    float population, percentage, threshold, amount_of_increase;
+    int threshold_value = 0;
+    int year = 0;
+
+    printf("Enter the population:\n");
+    scanf("%f", &population);
+
+    printf("Enter the percentage:\n");
+    scanf("%f", &percentage);
+
+    printf("Enter the threshold:\n");
+    scanf("%f", &threshold);
+    printf("\n");
+    while (population <= threshold) {
+        amount_of_increase = (population * percentage) / 100;
+        population += amount_of_increase;
+        population = round(population);
+        threshold_value++;
+        year++;
+        printf("amount of increase: %.2f, population after %d. year: %.0f\n", amount_of_increase,year,  population);
+
+    }
+
+    printf("\nTotal years to surpass the threshold value: %d\n", threshold_value);
